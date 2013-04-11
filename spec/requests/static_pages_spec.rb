2633f114781af05d7e1cce33832005f0a5677b12
Jay Danielian <jaydanielian@new-host.home>
2013-04-11 00:08:57 -0400

    adds spec title helper full_title. refactor for cleanup on rspec tests wioth shorter syntax

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,7 +15,7 @@ subject { page }
 
     it { should have_selector('h1', text: 'Sample App')}
 
-    it { should have_selector 'title', text: "Ruby on Rails Tutorial Sample App" }
+    it { should have_selector 'title', text: full_title('') }
  
    it { should_not have_selector 'title', :text => '| Home'}
  
@@ -26,46 +26,24 @@ subject { page }
 describe "Help page" do
 	before { visit help_path}
 
-	it "should have the h1 'Help'" do
-
-		page.should have_selector('h1', :text => 'Help' )
-
-		end
-
-	it "should have the title 'Help'" do
-
-		page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
-	end
+	it  { should have_selector('h1', text: 'Help')}
+	it { should have_selector('title', text: full_title('Help'))}
 
 	end
 
 describe "About page" do
 	before{ visit about_path}
 
-	it "should have the h1 'About Us'" do
-		page.should have_selector('h1', :text => 'About Us')
-	end
-
-	it "should have the title 'About Us'" do
-
-		page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
-	end
-
+it { should have_selector('h1', text:'About Us')}
+it {should have_selector('title', text: full_title('About Us'))}
 
 end
 
 describe "Contact page" do
 before { visit contact_path }
 
-	it "should have the h1 'Contact'" do
-		page.should have_selector('h1', text: 'Contact')
-	end
-
-
-	it "should have the title 'Contact'" do
-		page.should have_selector('title', text: "#{base_title} | Contact")
-
-end
+it { should have_selector('h1', text: 'Contact')}
+it {should have_selector('title', text: full_title('Contact'))}
 
 
 end
