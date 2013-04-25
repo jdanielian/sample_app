e64db91d9e1aa79d22d575fe86af50f1868d6d02
Jay Danielian <jaydanielian@new-host.home>
2013-04-25 17:50:04 -0400

    finished ch 7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -35,7 +35,21 @@ end
 				expect { click_button submit}.not_to change(User, :count)
 			end
 
+			describe "after submission" do
+				before {click_button submit}
+
+				it {should have_selector('title', text: 'Sign up')}
+				it {should have_content('error')}
+				it {should have_content("Name can't be blank")}
+				it {should have_content("Password can't be blank")}
+				it {should have_content("Email is invalid")}
+				it {should have_content("Password is too short")}
+				it {should_not have_content("Password digest")}
+			end
+
 		end
+
+
 
 		describe "with valid information" do
 			before do 
@@ -47,6 +61,14 @@ end
 			it "should create a user" do 
 				expect {click_button submit }.to change(User, :count).by(1)
 			end 
+
+			describe "after saving the user" do
+				before {click_button submit}
+				let(:user) { User.find_by_email('user@example.com')}
+
+				it {should have_selector('title', text: user.name)}
+				it {should have_selector('div.alert.alert-success', text: 'Welcome')}
+			end
 
 		end
 
